b2b37e0f8e3aa0776d8e7c45a71cbfa8ad199ba3
Tandi Samples <tandisamples@gmail.com>
2019-09-15 18:16:06 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+  self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+   array = self.squeeze("!").squeeze(".").split(/[$.|?|!]/)
+    array.count
+  #binding.pry
   end
 end
